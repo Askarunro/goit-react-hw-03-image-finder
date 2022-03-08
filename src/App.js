cc963f1f6a8489d086edd0d2015974118ce12bbe
Askarunro <paskar.y.a@gmail.com>
2022-03-08 12:42:50 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { nanoid } from "nanoid";
+import React, { Component } from "react";
+
+
+class App extends Component {
+
+  state = {
+
+  };
+
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
